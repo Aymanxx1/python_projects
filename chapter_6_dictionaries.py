2ee0fc5edbcf13e609aaa19fdd1d86331598caa2
ayman <ayman.mohmed.emam@gmail.com>
2023-10-11 18:38:57 +0300

    polling

--- chapter_6_dictionaries.py
+++ chapter_6_dictionaries.py
@@ -21,3 +21,50 @@ print(f"mohamed's favourite number is {favourite_numbers['mohamed']}")
 print(f"khalad's favourite number is {favourite_numbers['khalad']}")
 
 print()
+#favourite numbers with loop 6-4
+favourite_numbers = {
+    "ayman": 5,
+    "ahmed": 10,
+    "medhat": 9,
+    "mohamed": 22,
+    "khalad": 17,
+     }
+for name, value in favourite_numbers.items():
+    print(f"{name.title()}'s favourite number is {value} .")
+print()
+favourite_numbers["emam"]= 20
+favourite_numbers["sobhy"]= 11
+for name, value in favourite_numbers.items():
+    print(f"{name.title()}'s favourite number is {value} .")
+
+print()
+#rivers 6-5
+rivers = {
+    "nile": "egypt",
+    "farat": "iraq",
+    "messasabe": "england",
+     }
+for river, country in rivers.items():
+    print(f"THe {river.title()} runs through {country.title()}")
+print()
+for river in rivers.keys():
+    print(river)    
+print()
+for country in rivers.values():
+    print(country)
+
+print
+#polling 6-6 
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'rust',
+'phil': 'python',
+}
+names = ["jen", "ayman","phil"]
+for name in names:
+    if name in favorite_languages.keys():
+        print(f"{name}, thanks for taking the poll")
+    else:
+        print(f"{name}, you have to take the poll")
+print()
