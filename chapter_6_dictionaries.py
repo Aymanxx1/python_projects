a84cc59cb1f00aa3390284046d5124982594f56b
ayman <ayman.mohmed.emam@gmail.com>
2023-10-11 17:35:36 +0300

    modified

--- chapter_6_dictionaries.py
+++ chapter_6_dictionaries.py
@@ -1,1 +1,23 @@
 #6-1. Person
+person = {"first name": "ayman", "last name": "mohamed", "age": 23, "city": "cairo"}
+print(person["first name"])
+print(person["last name"])
+print(person["age"])
+print(person["city"])
+
+print()
+#6-2. Favorite Numbers
+favourite_numbers = {
+    "ayman": 5,
+    "ahmed": 10,
+    "medhat": 9,
+    "mohamed": 22,
+    "khalad": 17,
+     }
+print(f"ayman's favourite number is {favourite_numbers['ayman']}")
+print(f"ahmed's favourite number is {favourite_numbers['ahmed']}")
+print(f"medhat's favourite number is {favourite_numbers['medhat']}")
+print(f"mohamed's favourite number is {favourite_numbers['mohamed']}")
+print(f"khalad's favourite number is {favourite_numbers['khalad']}")
+
+print()
